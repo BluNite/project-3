61b7ccbbb6bd415bb5f8aa25d9a2b498e337d63f
guamanEd <edwinsg96@gmail.com>
2021-08-31 20:55:27 -0400

    updating index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,5 @@
 const User = require('./User');
-<<<<<<< HEAD
-const Product = require('./Product');
-
-
-module.exports = { User, Product, };
-=======
 
 
 module.exports = { User };
->>>>>>> b97f4f6079e6fe58369d9206720da361acaac166
+ 
